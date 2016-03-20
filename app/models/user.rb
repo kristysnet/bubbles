62618ffbedc68b42f600d015c2f3f65aa2f9912d
Kristy Gateley <kristysnet@gmail.com>
2016-03-20 11:45:29 -0700

    Style fixes for signup and edit account pages Installed styles for flash notices

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ActiveRecord::Base
   has_many :labels
   has_many :accomplishments
 
-  validates_presence_of :email, :first_name, :last_name
-  validates_uniqueness_of :email
+  validates_presence_of :first_name, :last_name
+  # validates :email, uniqueness: true
 
   def name
     "#{first_name} #{last_name}"
